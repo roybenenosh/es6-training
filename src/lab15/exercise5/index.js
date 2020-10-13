0f3176cbb3dd53ffa05c06d633d30ddcdbbc1613
rbenenosh <roybenenosh@ebay.com>
2020-10-14 00:44:45 +0300

    Correct the last asnwer

--- src/lab15/exercise5/index.js
+++ src/lab15/exercise5/index.js
@@ -1,9 +1,8 @@
-// the difference is the 1st method is a void and the 2nd is a returning statement
-// if we return the value of arr1.push(50) we will get undefined
 let arr1 = [10, 20, 30, 40];
 let arr2 = [10, 20, 30, 40];
 
-// arr = arr1.push(50);
+// first option  - extends the memory
+// second option - uses new memory
 arr1.push(50);
 arr2 = [...arr2, 50];
 
